5a8f89970ccf5d57fd238582aa709485ecf665c3
Ikey Doherty <ikey@serpentos.com>
2022-12-11 18:03:47 +0000

    base: Add Docker config
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ base/mkdocker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+if [[ "$EUID" -ne "0" ]]; then
+    echo "Must be run as root."
+    exit 1
+fi
+
+rm -rf rootDir
+moss ar -D rootDir protosnek https://dev.serpentos.com/protosnek/x86_64/stone.index
+moss it -D rootDir -y moss bash dash nss curl
+
+# TODO: Stateless profile! This is ugly as sin.
+mkdir rootDir/root
+echo "root:x:0:0:root:/root:/bin/bash" >> rootDir/etc/passwd
+echo "root:x:0:0:root:/root:/bin/bash" >> rootDir/etc/passwd-
+echo "root:x:0:" >> rootDir/etc/group
+echo "root:x:0:" >> rootDir/etc/group-
+mkdir rootDir/tmp
+
+install -m 00644 profile rootDir/etc/profile
+
+# Clear the cache..
+rm -rf rootDir/.moss/cache/downloads
+docker rmi serpentos/base:latest || :
+
+docker build --tag serpentos/base:latest .
